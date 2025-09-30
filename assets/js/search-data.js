16494825eacf11b04da7dc6d4c5bcc09cd0bdb28
dibyendumishra <dibyendumishra96@gmail.com>
2025-09-30 17:42:59 +0000

    Deploying to gh-pages from @ dibyendumishra/dibyendumishra.github.io@d14d621f6de998822d6405ef6266b436f23ce918 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -379,11 +379,11 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-empire-of-ai",
+          title: 'Empire of AI',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/books/empire_of_AI/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
